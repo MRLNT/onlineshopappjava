a7f212092935e28b9678e766d321eb5090f7fa54
MRLNT <marcelantonio246@gmail.com>
2025-08-05 13:20:41 +0700

    settings project and add depedency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    implementation("com.google.code.gson:gson:2.9.1")
 }
